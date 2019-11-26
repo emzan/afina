faa56180f747d35045496ec332e161885ecffa2f
emzan <zanettiemilio@hotmail.com>
2019-11-26 21:26:14 +0800

    prima

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +10,44 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  loaded: boolean = false;
+  displayedColumns: string[] = [];
+  dataSource: MatTableDataSource<AnimationPlayState>;
+  symbol: any ;
+  url = 'https://financialmodelingprep.com/api/v3/company/historical-discounted-cash-flow/AAPL';
+  dcf: any ;
+  stockprice: any ;
+  date: any;
+  ineff: any;
+  perc: any;
+  zero: any;
+  uno: any;
+  percuno: any;
+  
+
+  
+
+
+  constructor(private http: HttpClient) {
+
+    this.http.get<any>(this.url).subscribe(data => {
+    
+
+      this.dcf = data.DCF;
+      this.stockprice = data["Stock Price"];
+      this.date = data.date;
+      this.symbol = data.symbol;
+      this.ineff = this.dcf - this.stockprice;
+      this.zero = this.dcf / 100;
+      this.perc = this.ineff / this.zero;
+      this.uno = this.stockprice / 100;
+      this.percuno = this.ineff / this.uno
+      
+      
+      
+    })
+  }
+
+  
 
 }
