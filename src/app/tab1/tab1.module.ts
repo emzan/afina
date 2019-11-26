7b603791dcd03a7bf9e2316e9286e15bd36ba9c7
minCai <zanettiemilio@hotmail.com>
2019-11-27 00:01:18 +0800

    all inizio di nuovo che pena

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -2,30 +2,16 @@ import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatNativeDateModule} from '@angular/material';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
-import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab1Page }]),
-    BrowserAnimationsModule,
-    FormsModule,
-    HttpClientModule,
-    
-    MatNativeDateModule,
-    ReactiveFormsModule,
+    RouterModule.forChild([{ path: '', component: Tab1Page }])
   ],
-  declarations: [Tab1Page],
-  entryComponents: [Tab1Page],
-  bootstrap: [Tab1Page],
-  providers: []
+  declarations: [Tab1Page]
 })
 export class Tab1PageModule {}
