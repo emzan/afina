b18d9c998473e7bfce3b084aecc9c00b8bb4460f
emzan <zanettiemilio@hotmail.com>
2019-11-26 23:30:06 +0800

    proviamo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,15 +11,27 @@ import {MatNativeDateModule} from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(), AppRoutingModule,  MatNativeDateModule],
+  imports: [BrowserModule, 
+    HttpClientModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,  
+    MatNativeDateModule],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, 
+      useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -2,16 +2,30 @@ import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import { Tab1Page } from './tab1.page';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab1Page }])
+    RouterModule.forChild([{ path: '', component: Tab1Page }]),
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    
+    MatNativeDateModule,
+    ReactiveFormsModule,
   ],
-  declarations: [Tab1Page]
+  declarations: [Tab1Page],
+  entryComponents: [Tab1Page],
+  bootstrap: [Tab1Page],
+  providers: []
 })
 export class Tab1PageModule {}
