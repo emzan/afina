842e319aca36aefa997cc20eac82945d0eec9fd7
emzan <zanettiemilio@hotmail.com>
2019-11-27 15:01:46 +0800

    prova Intera

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,52 +1,52 @@
-
-import { Component, ViewChild } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 import { HttpClient } from '@angular/common/http';
-
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
-
+export class Tab3Page implements OnInit {
   loaded: boolean = false;
   displayedColumns: string[] = [];
   dataSource: MatTableDataSource<AnimationPlayState>;
-  symbol: any ;
-  url = 'https://financialmodelingprep.com/api/v3/company/discounted-cash-flow/AAPL';
-  dcf: any ;
-  stockprice: any ;
-  date: any;
-  ineff: any;
-  perc: any;
-  zero: any;
-  uno: any;
-  percuno: any;
-  
-
-  
-
+  symbol = 'AAPL';
+  url = 'https://financialmodelingprep.com/api/v3/company/stock/list';
+  financialStatement: any = [];
+  @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: false}) sort: MatSort;
 
   constructor(private http: HttpClient) {
 
     this.http.get<any>(this.url).subscribe(data => {
-    
+      this.financialStatement = data.symbolsList.slice(0,500);
+      this.createdDisplayComlumn(this.financialStatement[0]);
 
-      this.dcf = data.DCF;
-      this.stockprice = data["Stock Price"];
-      this.date = data.date;
-      this.symbol = data.symbol;
-      this.ineff = this.dcf - this.stockprice;
-      this.zero = this.dcf / 100;
-      this.perc = this.ineff / this.zero;
-      this.uno = this.stockprice / 100;
-      this.percuno = this.ineff / this.uno
-      
-      
-      
+      this.dataSource = new MatTableDataSource(this.financialStatement);
+      this.loaded =true;
     })
   }
 
+  ngOnInit() {
+    setTimeout(() => {
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    },1000);
+    
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  createdDisplayComlumn(obj) {
+    Object.keys(obj).forEach((key,index)=> {
+      this.displayedColumns.push(key);
+    });
+  }
 }
